7ee5993102a0559fdc8b7db1c8726a5fc5086b18
Manjeetdhimaan <manjeetdhimaan80@gmail.com>
2022-11-24 15:40:38 +0530

    initial commit

---
+++ modal.js
@@ -0,0 +1,33 @@
+const modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+// const btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+const span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+// btn.onclick = function() {
+//   modal.style.display = "block";
+// }
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    // modal.style.display = "block";
+    setTimeout(() => {
+        modal.style.display = "block";
+    }, 1500);
+ }, false);
+
+
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
