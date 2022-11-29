0b5678ea75b781b27a2fd62073a700cae93cf155
Manjeetdhimaan <manjeetdhimaan80@gmail.com>
2022-11-29 11:25:07 +0530

    updated code pages

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -30,4 +30,36 @@ router.post('/send-mail',  (req, res, next) => {
       });
 });
 
+router.post('/send-mail-everday-sol',  (req, res, next) => {
+  const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: 'manjeetdhimaan60@gmail.com',
+        pass: 'lpaqbtmffjmepylc'
+      }
+    });
+    
+    const mailOptions = {
+      from: 'youremail@gmail.com',
+      to: 'manjeetdhimaan80@gmail.com',
+      subject: 'Email from ' + req.body.domain,
+      text: `Name: ${req.body.name ? req.body.name : ''} ,
+              Email: ${req.body.email ? req.body.email : ''}
+              Subject: ${req.body.sub ? req.body.sub : ''}
+              Message: ${req.body.msg ? req.body.msg : ''}
+      `
+    };
+    
+     transporter.sendMail(mailOptions, function(error, info){
+      if (error) {
+        console.log(error);
+        res.send({error: error})
+      } else {
+          console.log('Email sent: ' + info.response);
+          res.send({res: info.response, message: 'E-mail sent successfully'})
+        
+      }
+    });
+});
+
 module.exports = router;
